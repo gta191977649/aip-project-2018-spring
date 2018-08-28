dba25d53b6ae481021f528711dfd9359f7f1af04
IdrisDose <idrisdev@gmail.com>
2018-08-28 04:16:09 -0700

    Fixed Docker MySQL - Changed IP to 172.21.0.1 - for some reason that's where the MySQL is hosted?

--- server/config/datastores.js
+++ server/config/datastores.js
@@ -1,3 +1,4 @@
+require('sails-mysql')
 /**
  * Datastores
  * (sails.config.datastores)
@@ -48,8 +49,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: 'mysql://root:secret@172.21.0.1:3306/storeapp'
 
   },
 
