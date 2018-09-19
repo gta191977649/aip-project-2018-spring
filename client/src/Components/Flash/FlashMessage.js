45dc3be54fff35bd6b36a05f83cae3a5f5c2acf7
Idris Dose <idrisdev@gmail.com>
2018-09-19 23:20:55 +1000

    Fixed Types eroror

--- client/src/Components/Flash/FlashMessage.js
+++ client/src/Components/Flash/FlashMessage.js
@@ -2,13 +2,20 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 import {deleteFlashMessage} from '../../Actions/FlashActions';
-
+import ProgressBar from '../ProgressBar';
 export class FlashMessage extends Component { 
   onClick(event){
     //TODO: Change to remove single later
     this.props.deleteFlashMessage(this.props.message.id);
   }
+  async componentDidMount(){
+    await this.sleep(2000);
+    this.onClick(null);
+  }
 
+  async sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
   render() {
     const { type, text } = this.props.message;
 
@@ -21,10 +28,12 @@ export class FlashMessage extends Component {
         })}
         role="alert"
       >
+      
         <button onClick={this.onClick.bind(this)} className="close">
           &times;
         </button>
         {text}
+        {/* <ProgressBar progress={0} autoIncrement={true} interval={10}/> */}
       </div>
     );
   }
