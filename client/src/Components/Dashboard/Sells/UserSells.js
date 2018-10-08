2411aa370ee7f9003d98d6e046917274a99db772
yamato2199 <sonic19951220@hotmail.com>
2018-10-08 17:47:39 +1100

    sperate sell and orders

--- client/src/Components/Dashboard/Sells/UserSells.js
+++ client/src/Components/Dashboard/Sells/UserSells.js
@@ -1,12 +1,12 @@
 import React from "react";
 import {Component} from "react";
-import UserOrderItem from './UserOrderItem';
-import {fetchOrderByUserId} from "../../../Actions/OrderAction";
+import UserSellItem from './UserSellItem';
+import {fetchOrderByUserId, fetchOrderBySellerId} from "../../../Actions/OrderAction";
 import {addOrder} from "../../../Actions/OrderAction";
 import connect from "react-redux/es/connect/connect";
 import { Button,Collapse } from 'mdbreact'
 
-class UserOrders extends Component{
+class UserSells extends Component{
   constructor(props) {
     super(props);
     this.state = {
@@ -14,7 +14,7 @@ class UserOrders extends Component{
     };
     this.removeData = this.removeData.bind(this);
     this.toogleEdit = this.toogleEdit.bind(this);
-    this.fetchOrderData = this.fetchOrderData.bind(this);
+    this.fetchOrderSellerData = this.fetchOrderSellerData.bind(this);
   }
   toogleEdit(itemData=null) {
     if(itemData!=null) {
@@ -34,20 +34,21 @@ class UserOrders extends Component{
   componentDidMount() {
     //Set fake data
     //console.log(this.state.products);
-    this.fetchOrderData(this.props.userID);
+    this.fetchOrderSellerData(this.props.userID);
   }
 
-  fetchOrderData(userId) {
-    this.props.fetchOrderByUserId(userId);
+  fetchOrderSellerData(sellerId){
+    this.props.fetchOrderBySellerId(sellerId);
   }
+
   removeData(id) {
     this.setState({
       orders:this.state.orders.filter(item => item.id !== id)
     });
   }
   componentWillReceiveProps(newProps) {
-      //console.log(newProps.userOrders);
-    this.setState({orders: newProps.userOrders});
+    //console.log(newProps.userOrders);
+    this.setState({orders: newProps.sellerOrders});
   }
 
   render () {
@@ -55,24 +56,24 @@ class UserOrders extends Component{
     const orderItems = this.state.orders.map((item,idx) =>(
       <div className="col-md-12" key={idx}>
 
-        <UserOrderItem data={item}  parentRemove={this.removeData}/>
+        <UserSellItem data={item}  parentRemove={this.removeData}/>
 
       </div>
     ));
     return(
       <div className="div">
-          <h1>My orders <small className="text-info">{this.state.orders.length}</small></h1>
-          <hr/>
+        <h1>My Sells <small className="text-info">{this.state.orders.length}</small></h1>
+        <hr/>
 
-      {orderItems}
+        {orderItems}
       </div>
     );
   }
 }
 const mapStateToProps = state => ({
-    userOrders: state.orders.userOrders,
-    userID:state.auth.user.id,
-    error: state.products.error,
-    errorMsg: state.products.errorMsg
+  sellerOrders: state.orders.sellerOrders,
+  userID:state.auth.user.id,
+  error: state.products.error,
+  errorMsg: state.products.errorMsg
 });
-export default connect(mapStateToProps, {fetchOrderByUserId,addOrder})(UserOrders);
+export default connect(mapStateToProps, {fetchOrderBySellerId,addOrder})(UserSells);
