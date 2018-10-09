0d4dcefba3ddab6aaaf0ed9526f845591ac60c7b
ヌルポ <Le.Cai-1@student.uts.edu.au>
2018-10-09 19:24:24 +1100

    fill the info to the about page

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -9,10 +9,14 @@ export default class About extends Component {
     return (
       <Container>
         <Row>
-          <div className="container">
+          <div className="container text-center">
             <h1 className="display-4 text-center">About</h1>
               <hr/>
-              
+              <h2>This Project is developed by</h2>
+              <p>Idris Dev</p>
+              <p>Wenze Jiang</p>
+              <p>Le Cai</p>
+              <p className="text-info">For <strong>Advanced internet programming Spring 2018</strong></p>
           </div>
         </Row>
       </Container>
