5901699253f27a824db527b0b443024de29a38ad
Idris Dose <idrisdev@gmail.com>
2018-10-13 20:48:40 +1100

    Removed Tabs and replaced with side-nav function so other buttons can directly access the dashboard pages.

--- client/src/Components/Dashboard/DashboardPage.js
+++ client/src/Components/Dashboard/DashboardPage.js
@@ -1,42 +1,25 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
-import {
-  Container,
-  Row,
-  Col,
-  TabPane,
-  TabContent,
-  Nav,
-  NavItem,
-  NavLink,
-  Fa
-} from "mdbreact";
-import classnames from "classnames";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Container, Row, Col, Nav, NavItem, NavLink, Fa } from "mdbreact";
 
 //Components
-import UserSells from "./Sells/UserSells";
 import ProfileForm from "../Auth/Profile/ProfileForm";
+import DashboardHomePage from "./DashboardHomePage";
 import OrderList from "../Order/OrderList";
+import isEmpty from "../../Utils/isEmpty";
 
 export class DashboardPage extends Component {
-  static propTypes = {};
+  static propTypes = {
+    auth: PropTypes.object.isRequired
+  };
+
   constructor(props) {
     super(props);
-
-    this.toggleVerticalPills = this.toggleVerticalPills.bind(this);
     this.state = {
-      activeItemVerticalPills: "1"
+      activeItem: ""
     };
-  }
-
-  toggleVerticalPills(tab) {
-    if (this.state.activeItem3 !== tab) {
-      this.setState({
-        activeItemVerticalPills: tab
-      });
-    }
   }
 
   render() {
@@ -44,76 +27,56 @@ export class DashboardPage extends Component {
       <Router>
         <Container className="mt-5" fluid>
           <Row className="pt-5">
-            <Col md="12">
-              <Row>
-                <Col md="2">
-                  <h3 className="text-center">Dashboard Navigation</h3>
-                  <hr />
-                  <Nav pills color="blue" className="flex-column">
-                    <NavItem>
-                      <NavLink
-                        to="#"
-                        className={classnames({
-                          active: this.state.activeItemVerticalPills === "1"
-                        })}
-                        onClick={() => {
-                          this.toggleVerticalPills("1");
-                        }}
-                      >
-                        Buying
-                        <Fa icon="cart-arrow-down" className="ml-2" />
-                      </NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink
-                        to="#"
-                        className={classnames({
-                          active: this.state.activeItemVerticalPills === "2"
-                        })}
-                        onClick={() => {
-                          this.toggleVerticalPills("2");
-                        }}
-                      >
-                        Selling
-                        <Fa icon="dollar" className="ml-2" />
-                      </NavLink>
-                    </NavItem>
-                    <NavItem>
-                      <NavLink
-                        to="#"
-                        className={classnames({
-                          active: this.state.activeItemVerticalPills === "3"
-                        })}
-                        onClick={() => {
-                          this.toggleVerticalPills("3");
-                        }}
-                      >
-                        Settings <Fa icon="gear" className="ml-2" />
-                      </NavLink>
-                    </NavItem>
-                  </Nav>
-                  <hr />
-                </Col>
-                <Col md="9">
-                  <TabContent
-                    activeItem={this.state.activeItemVerticalPills}
-                    className="pt-0"
-                  >
-                    {/* Orders */}
-                    <TabPane tabId="1">
-                      <OrderList />
-                    </TabPane>
-                    {/* Sales */}
-                    <TabPane tabId="2">
-                      <UserSells />
-                    </TabPane>
-                    {/* Settings */}
-                    <TabPane tabId="3">
-                      <ProfileForm />
-                    </TabPane>
-                  </TabContent>
-                </Col>
-              </Row>
+            <Col md="2">
+              <h3 className="text-center">Dashboard Navigation</h3>
+              <hr />
+              <Nav pills color="blue" className="flex-column">
+                <NavItem>
+                  <NavLink to="/dashboard/home">
+                    Dashboard Home
+                    <Fa icon="dashboard" className="ml-2" />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="/dashboard/buying">
+                    Buying
+                    <Fa icon="cart-arrow-down" className="ml-2" />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="/dashboard/selling">
+                    Selling
+                    <Fa icon="dollar" className="ml-2" />
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink to="/dashboard/settings">
+                    Settings
+                    <Fa icon="gear" className="ml-2" />
+                  </NavLink>
+                </NavItem>
+              </Nav>
+              <hr />
+            </Col>
+            <Col md="9">
+              <Switch>
+                <Route
+                  exact
+                  path="/dashboard/home"
+                  component={DashboardHomePage}
+                />
+                <Route exact path="/dashboard/buying" component={OrderList} />
+                <Route
+                  exact
+                  path="/dashboard/selling"
+                  component={ProfileForm}
+                />
+                <Route
+                  exact
+                  path="/dashboard/settings"
+                  component={ProfileForm}
+                />
+              </Switch>
             </Col>
           </Row>
         </Container>
@@ -122,7 +85,9 @@ export class DashboardPage extends Component {
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  auth: state.auth
+});
 
 const mapDispatchToProps = {};
 
