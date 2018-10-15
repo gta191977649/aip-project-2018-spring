78ff1e6fdfe792f2ca933b681e860b7477d25719
Idris Dose <idrisdev@gmail.com>
2018-10-16 01:48:29 +1100

    Added new fields to product, Qty to keep track of quantity, added category and image.

--- server/server/models/Product.js
+++ server/server/models/Product.js
@@ -1,32 +1,55 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let ProductSchema = new Schema({
   seller: {
     type: Schema.Types.ObjectId,
-    ref: "User"
+    ref: 'User',
+    required: true,
   },
-  category: {
+  link: {
     type: String,
-    required: true
+    required: true,
   },
   name: {
     type: String,
-    required: true
+    required: true,
   },
   price: {
     type: Number,
-    required: true
+    required: true,
   },
-  img: {
-    type: Array,
-    required: true
+  category: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
   },
   description: {
     type: String,
-    require: true
-  }
+    require: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Getter
+ProductSchema.path('price').get(function(num) {
+  return (num / 100).toFixed(2);
+});
+
+// Setter
+ProductSchema.path('price').set(function(num) {
+  return num * 100;
 });
 
 // eslint-disable-next-line no-undef
-module.exports = Product = mongoose.model("Product", ProductSchema);
+module.exports = Product = mongoose.model('Product', ProductSchema);
