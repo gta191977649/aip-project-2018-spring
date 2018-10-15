ecc4917e8ada41774f7b4c609a69333b24931620
Idris Dose <idrisdev@gmail.com>
2018-10-16 01:46:51 +1100

    Created Skeleton file for OrderActions, SellingList and ProductInfo components

---
+++ client/src/Components/Products/ProductInfo.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+export class ProductInfo extends Component {
+  static propTypes = {
+    prop: PropTypes
+  };
+
+  render() {
+    return <div />;
+  }
+}
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = {};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProductInfo);
