8d439296a8f23345ce9f41fb1aba42c5925e0cc6
Idris Dose <idrisdev@gmail.com>
2018-10-16 01:42:43 +1100

    Added new util functions on the server check-mongoose-connection returns true if mongoose is not connected slugify converts a string to a url friendly string.

---
+++ server/server/utils/check-mongoose-connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+  let connected = mongoose.connection.readyState !== 1;
+  console.log('Is Mongoose Connected? ' + !connected);
+  return connected;
+};
