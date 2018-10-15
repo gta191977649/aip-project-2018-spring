772fe4b06162a9cc9f609229d0a76e13349f411c
Idris Dose <idrisdev@gmail.com>
2018-10-16 01:46:16 +1100

    created Skeleton feedback components.

---
+++ client/src/Components/Profile/FeedbackStars.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Fa } from "mdbreact";
+export default class FeedbackItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      positiveCount: 0,
+      neutrualCount: 0,
+      negativeCount: 0
+    };
+  }
+
+  componentDidMount() {
+    let posCount = 0;
+    let neutrualCount = 0;
+    let negativeCount = 0;
+    this.props.feedback.map();
+  }
+
+  render() {
+    let star = <Fa icon="star" />;
+    let halfStar = <Fa icon="star-half-full" />;
+    let emptyStar = <Fa icon="star-o" />;
+
+    return <div />;
+  }
+}
