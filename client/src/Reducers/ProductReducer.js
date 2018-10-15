9b9d8aff601a1f5f0419ab1a2e219ba038e6a8d2
Idris Dose <idrisdev@gmail.com>
2018-10-16 01:40:02 +1100

    Remvoed Redundant Actions

--- client/src/Reducers/ProductReducer.js
+++ client/src/Reducers/ProductReducer.js
@@ -1,55 +1,16 @@
-import * as Type from "../Actions/Types";
-import { UPDATE_PRUDUCT } from "../Actions/Types";
+import { NEW_PRODUCT } from "../Actions/Types";
 const initialState = {
-  items: [],
-  item: {},
-  error: false,
-  errorMsg: '',
-}
+  products: []
+};
 
-export default function (state = initialState, action) {
+export default function(state = initialState, action) {
   switch (action.type) {
-    case Type.FETCH_PRODUCTS:
+    case NEW_PRODUCT:
       return {
         ...state,
-        items: action.payload
-      }
-    case Type.FETCH_PRODUCT_ID:
-      return {
-        ...state,
-        productItem: action.payload
-      }
-    case Type.SEARCH_PRODUCTS:
-      return {
-        ...state,
-        items: action.payload
-      }
-    case Type.FETCH_PRODUCT_USER_ID:
-      return {
-        ...state,
-        userItems: action.payload
-      }
-    case Type.ADD_PRUDUCT:
-      return {
-        ...state,
-        addResponse: action.payload
-      }
-    case Type.DELETE_PRODUCT:
-      return {
-        ...state,
-        delResponse: action.payload
-      }
-    case Type.UPDATE_PRUDUCT:
-      return {
-        ...state,
-        updateResponse: action.payload
-      }
-    case Type.FETCH_PRODUCTS_ERROR:
-      return {
-        ...state,
-        error: true,
-        errorMsg: action.payload,
-      }
-    default: return state;
+        products: state.products.push(action.payload)
+      };
+    default:
+      return state;
   }
 }
