b881df11b5da422659c86a596f91ac343f34a104
Idris Dose <idrisdev@gmail.com>
2018-10-16 01:49:28 +1100

    Changed UserController to use new check Mongoose connection function and encased main actions in try catch for better error handling.

--- server/server/controllers/UserController.js
+++ server/server/controllers/UserController.js
@@ -4,6 +4,7 @@ const User = require('../models/Account');
 const Profile = require('../models/UserProfile');
 const validateRegister = require('../validation/validateRegister');
 const validateLogin = require('../validation/validateLogin');
+const mongoNotConnected = require('../utils/check-mongoose-connection');
 
 // @route POST api/auth/current
 // @desc Gets current user details
@@ -22,59 +23,63 @@ module.exports.user_current = async (req, res) => {
 module.exports.user_register = async (req, res) => {
   const {errors, isValid} = validateRegister(req.body);
   if (!isValid) {
-    console.log('Not valid yo' + JSON.stringify(errors));
-    return res.status(400).json({
-      errors,
-    });
+    return res.status(400).json({errors});
   }
 
-  let user = await User.findOne({email: req.body.email});
-  if (user) {
-    errors.email = 'Email already exists';
-    console.log('ERROR: email exists');
-    return res.status(400).json({errors});
-  } else {
-    let username = await User.findOne({handle: req.body.handle});
-    if (username) {
-      errors.username = 'Username already exists';
-      console.log('ERROR: username/handle exists!');
+  try {
+    // Check if mongo is connected
+    if (mongoNotConnected()) {
+      errors.message = 'Database not connected, contact server administrator';
       return res.status(400).json({errors});
     }
 
-    let avatar = gravatar.url(req.body.email, {
-      s: '200', // Size
-      r: 'pg', // Rating
-      d: 'mm', // Default
-    });
+    let user = await User.findOne({email: req.body.email});
+    if (user) {
+      errors.email = 'Email already exists';
+      console.log('ERROR: email exists');
+      return res.status(400).json({errors});
+    } else {
+      let username = await User.findOne({handle: req.body.handle});
+      if (username) {
+        errors.username = 'Username already exists';
+        console.log('ERROR: username/handle exists!');
+        return res.status(400).json({errors});
+      }
 
-    let newUser = new User({
-      handle: req.body.handle,
-      fname: req.body.fname,
-      lname: req.body.lname,
-      name: req.body.fname + ' ' + req.body.lname,
-      email: req.body.email,
-      avatar,
-      password: req.body.password,
-    });
-    bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-            .save()
-            .then((user) => {
-              let profile = new Profile({user: user._id});
-              profile.save(function(err) {
-                if (err) console.log(err);
-              // thats it!
-              });
-              res.json({
-                success: true,
-              });
-            })
-            .catch((err) => console.log(err));
+      let avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm', // Default
       });
-    });
+
+      let newUser = new User({
+        handle: req.body.handle,
+        fname: req.body.fname,
+        lname: req.body.lname,
+        name: req.body.fname + ' ' + req.body.lname,
+        email: req.body.email,
+        avatar,
+        password: req.body.password,
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser.save().then((user) => {
+            let profile = new Profile({user: user._id});
+            profile.save(function(err) {
+              if (err) console.log(err);
+              // thats it!
+            });
+            res.json({
+              success: true,
+            });
+          });
+        });
+      });
+    }
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -88,23 +93,33 @@ module.exports.user_login = async (req, res) => {
   }
   const {email, password} = req.body;
 
-  // Find user by email
-  let user = await User.findOne({email}).catch((error) => console.log(error));
-  if (!user) {
-    errors.email = 'User not found';
-  } else {
-    // Check Password
-    let matched = await bcrypt
-        .compare(password, user.password)
-        .catch((error) => console.log(error));
-    if (matched) {
-      return res.json({
-        success: true,
-        token: user.generateJWT(),
-      });
-    } else {
-      errors.password = 'Password incorrect';
+  try {
+    // Check if mongo is connected
+    if (mongoNotConnected()) {
+      errors.message = 'Database not connected, contact server administrator';
+      return res.status(400).json({errors});
     }
+    // Find user by email
+    let user = await User.findOne({email});
+    console.log('TEST1');
+    if (!user) {
+      errors.email = 'User not found';
+    } else {
+      // Check Password
+      let matched = await bcrypt
+          .compare(password, user.password)
+          .catch((error) => console.log(error));
+      if (matched) {
+        return res.json({
+          success: true,
+          token: user.generateJWT(),
+        });
+      } else {
+        errors.password = 'Password incorrect';
+      }
+    }
+  } catch (err) {
+    console.log(err);
   }
 
   // Decision to put this here was because it's a shorter method than registration
