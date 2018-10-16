90720f7427dc1fd141bf9d33c4cc7424b776d49e
Idris Dose <idrisdev@gmail.com>
2018-10-16 19:51:02 +1100

    Created Default User information

--- server/server/config/keys.js
+++ server/server/config/keys.js
@@ -1,6 +1,14 @@
 'use strict';
+const shortid = require('shortid');
 
 module.exports = {
   mongo: process.env.MONGO_URL || 'mongodb://localhost:27017/storeapp_aip',
   secret: process.env.SECRET || 'S3CR3T',
+  defaultUser: {
+    fname: 'AIP',
+    lname: 'Auctions',
+    email: 'aipauctions@example.com',
+    password: shortid.generate(),
+    handle: 'aip-auctions',
+  },
 };
