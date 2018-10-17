49808ee4edf72f2bc56bb6a4ac157640bc9295dc
Idris Dose <idrisdev@gmail.com>
2018-10-18 03:12:08 +1100

    Added history object to reset window scroll to top on route change.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router } from "react-router-dom";
+import { Router } from "react-router-dom";
+import createHistory from "history/createBrowserHistory";
+
 //MDB
 import "font-awesome/css/font-awesome.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
@@ -20,9 +22,16 @@ import OrdersComponent from "./Components/StorageComponents/OrderComponent";
 
 class App extends Component {
   render() {
+    const history = createHistory();
+
+    // Dirty Fix to scroll back to top on route changed :D
+    history.listen((location, action) => {
+      window.scroll(0, 0);
+    });
+
     return (
       <Provider store={store}>
-        <Router>
+        <Router history={history}>
           <div className="App">
             <AuthComponent />
             <ProductsComponent />
