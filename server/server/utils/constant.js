cb8171fdde685ccb224fbea92e21ab3c2319efe3
東郷毅 <sonic19951220@hotmail.com>
2018-10-19 01:41:03 +1100

    convert message in product controller into constant

--- server/server/utils/constant.js
+++ server/server/utils/constant.js
@@ -2,4 +2,6 @@ module.exports = Object.freeze({
   CART_EMPTY: 'Cart cannot be empty',
   NOT_FOUND: 'Sorry can\'t found that',
   NOT_VALID_USER: 'No valid user found.',
+  UNKNOW_ERROR: 'Unknown error occured; try again later.',
+  NO_PRODUCT_LINK: 'Unable to proccess request; No product link provided.',
 });
