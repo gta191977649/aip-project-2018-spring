63b33a999a4ecbd552b3a7e6c5c7e98e246a769c
Idris Dose <idrisdev@gmail.com>
2018-10-20 00:10:57 +1100

    Automatically added and redirected to product page upon new product

--- client/src/Actions/ProductActions.js
+++ client/src/Actions/ProductActions.js
@@ -2,21 +2,27 @@ import Axios from "axios";
 
 import { FETCH_PRODUCTS, NEW_PRODUCT } from "./Types";
 import { handleError } from "./ErrorActions";
+import {toast} from 'mdbreact';
 
 const productsURL =
   process.env.REACT_APP_API_URL + "/products" || "/api/products";
 
-export const newProduct = formData => dispatch => {
+export const newProduct = (formData, history) => dispatch => {
   return Axios.post(productsURL + "/new", formData, {
     headers: {
       "Content-Type": "multipart/form-data"
     }
   })
     .then(response => {
-      return dispatch({
-        type: NEW_PRODUCT,
-        payload: response.data.product
-      });
+      if (response.status === 200){
+        dispatch({
+          type: NEW_PRODUCT,
+          payload: response.data.product
+        });
+        toast.success("Created new product");
+        history.push("/product/"+response.data.product.link);
+      }
+      
     })
     .catch(axiosError => handleError(axiosError, dispatch));
 };
